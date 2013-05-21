5153b07e448b640a6cad7b55fe76e4b0e32a9929
Hack Reactor Students <github@hackreactor.com>
2013-05-20 17:27:31 -0700

    ok...

--- SQL/schema.sql
+++ SQL/schema.sql
@@ -3,11 +3,15 @@ CREATE DATABASE chat;
 USE chat;
 
 CREATE TABLE messages (
- /* Describe your table here.*/
 );
 
-/* You can also create more tables, if you need them... */
 
-/*  Execute this file from the command line by typing:
- *    mysql < schema.sql
- *  to create the database and the tables.*/
+
+-- /* You can also create more tables, if you need them... */
+
+-- /*  Execute this file from the command line by typing:
+--  *    mysql < schema.sql
+--  *  to create the database and the tables.*/
+
+
+-- GRANT ALL PRIVILEGES ON chat.* TO banjolina@localhost IDENTIFIED BY 'OKOKOK'
